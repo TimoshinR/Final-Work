8343d57ab8220822d95d82a2e98b4f95982be6c2
Roman <treovv@mail.ru>
2022-09-05 14:38:28 +0300

    Creat README file

---
+++ Task/Program.cs
@@ -0,0 +1,28 @@
+﻿void FillArray (string [] array)
+{
+     Console.Write($"полученный массив: [");
+     string[] arr = new string[array.Length];
+     for (int i=0; i<array.Length; i++)
+     {      
+        if(array[i].Length<=3){
+        arr[i]=array[i];;
+        Console.Write($" '{arr[i]}' "); 
+        }
+     }
+        Console.WriteLine($"]"); 
+}
+
+void PrintArray(string [] Array)
+{
+    Console.Write($"исходный массив: [");
+    for (int i=0; i<Array.Length; i++)
+    {
+        Console.Write($" '{Array[i]}' ");
+    }
+    Console.Write($"]");
+}
+
+string [] array = {"hello", "2", "world", ":-)"};
+
+FillArray(array);
+PrintArray(array);
